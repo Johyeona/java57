9b90cec53de98bbecf70cfad30917855bb4ebcb8
Johyeona <gusdk8523@naver.com>
2014-10-27 18:57:00 +0900

    dgsg

--- bitTest/src/bitTest/test2.java
+++ bitTest/src/bitTest/test2.java
@@ -7,6 +7,14 @@ public class test2 {
     if(args.length > 0)
     {
       System.out.println("현아짱짱짱");
-    }
+    System.out.println("조현아 그리고 전승민입니다. ");
+   if(args.length >0){
+  	 System.out.println("메롱");
+   }
+    
+    System.out.println("오호라...");
+
+    System.out.println("아 정말 수업은 길구나");
+  }
   }
 }
