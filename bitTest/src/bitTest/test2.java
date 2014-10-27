1ef0966b42d22d8046e4bd46aee313a6c1ce5763
ParkHwaRyong <jjamuncle@gmail.com>
2014-10-27 18:43:55 +0900

    수정

--- bitTest/src/bitTest/test2.java
+++ bitTest/src/bitTest/test2.java
@@ -4,7 +4,10 @@ public class test2 {
 
   public static void main(String[] args) {
     System.out.println("조현아");
-
+    
+    System.out.println("오호라...");
   }
-
+  public static void m(){
+    System.out.println("test..");
+  }
 }
