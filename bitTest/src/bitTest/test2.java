78f7ca0c4915f6ad6d75639235027e97b01fab8f
Seungmin <smair7@gmail.com>
2014-10-27 18:43:51 +0900

    나야

--- bitTest/src/bitTest/test2.java
+++ bitTest/src/bitTest/test2.java
@@ -4,7 +4,11 @@ public class test2 {
 
   public static void main(String[] args) {
     System.out.println("조현아");
-
+    System.out.println("아 정말 수업은 길구나");
+  }
+    public static void m(){
+    	System.out.println("7조화이팅");
+    }
   }
 
-}
+
