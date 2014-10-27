0a455070b0ab4efdd0809414dba2db0baa6f0c7c
Johyeona <gusdk8523@naver.com>
2014-10-27 18:43:57 +0900

    편집

--- bitTest/src/bitTest/test2.java
+++ bitTest/src/bitTest/test2.java
@@ -3,8 +3,13 @@ package bitTest;
 public class test2 {
 
   public static void main(String[] args) {
-    System.out.println("조현아");
+    System.out.println("현아");
 
+  }
+  
+  public static void m()
+  {
+    System.out.println("하나의 파일 같이 편집");
   }
 
 }
